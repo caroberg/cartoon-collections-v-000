c29bae030bad227a7639b9c2f5922e708c083ce8
Caroline Berg <carolineeberg@gmail.com>
2018-08-08 14:46:41 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,10 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  i = 0
+  all_the_dwarves = []
+  while i < dwarves.length
+    all_the_dwarves << yield(dwarves[i])
+    i += 1
+  end
 end
 
 def summon_captain_planet# code an argument here
